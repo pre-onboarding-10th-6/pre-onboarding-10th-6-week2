beb9843476ee24f3d3c6823afa9012c8f5c27ee7
LJJ <wjdrk70@gmail.com>
2023-05-05 01:09:08 +0900

    test vercel 36

--- src/api/search.ts
+++ src/api/search.ts
@@ -3,4 +3,5 @@ import { Disease } from '../types/disease'
 import { instance } from '.'
 
 export const getDiseases = (name: string): Promise<Disease[]> =>
-  instance.get('api/v1/search-conditions/', { params: { name } })
+  instance.get(`api/v1/search-conditions/?name=${encodeURIComponent(name)}`)
+
