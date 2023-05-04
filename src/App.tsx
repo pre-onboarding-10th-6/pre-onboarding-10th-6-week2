e47bc0d85f7a1263274592737973247d19a3afb0
wjdrk70 <58238859+wjdrk70@users.noreply.github.com>
2023-05-04 11:07:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Search from './components/Search'
+import GlobalStyle from './styles/GlobalStyle'
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Search />
+    </>
+  )
+}
+
+export default App
