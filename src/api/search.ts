de4eff163d213d13c37b6f4611e9ba484d3f7b22
LJJ <wjdrk70@gmail.com>
2023-05-05 12:41:23 +0900

    test vercel46

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,11 +1,10 @@
 import { Disease } from '../types/disease'
 
 import { instance } from '.'
-const isLocal = window.location.hostname === 'localhost'
-const proxy = isLocal ? '' : 'https://api.clinicaltrialskorea.com'
+
 
 export const getDiseases = (name: string): Promise<Disease[]> =>
-  instance.get(`${proxy}/api/v1/search-conditions/`, {
+  instance.get(`/api/v1/search-conditions/`, {
     params: { name },
     withCredentials: true, // CORS 오류 방지
   })
