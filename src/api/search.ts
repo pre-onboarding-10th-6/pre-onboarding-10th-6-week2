969b72b9a145521578b5e6b72e092a1fb853f9cf
LJJ <wjdrk70@gmail.com>
2023-05-05 12:28:59 +0900

    test vercel43

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,7 +1,11 @@
 import { Disease } from '../types/disease'
 
 import { instance } from '.'
-const proxy=window.location.hostname==='localhost'?'':'https://pre-onboarding-10th-6-week2.vercel.app'
-export const getDiseases = (name: string): Promise<Disease[]> =>
-instance.get(`${proxy}/api/v1/search-conditions/`, { params: { name } })
+const isLocal = window.location.hostname === 'localhost'
+const proxy = isLocal ? '' : 'https://pre-onboarding-10th-6-week2.vercel.app/proxy'
 
+export const getDiseases = (name: string): Promise<Disease[]> =>
+  instance.get(`${proxy}/api/v1/search-conditions/`, {
+    params: { name },
+    withCredentials: true, // CORS 오류 방지
+  })
