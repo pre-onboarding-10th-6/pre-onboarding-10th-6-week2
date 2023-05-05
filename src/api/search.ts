002a75e10c794a45163b119decf077789c407f8a
LJJ <wjdrk70@gmail.com>
2023-05-05 12:34:37 +0900

    test vercel45

--- src/api/search.ts
+++ src/api/search.ts
@@ -2,7 +2,7 @@ import { Disease } from '../types/disease'
 
 import { instance } from '.'
 const isLocal = window.location.hostname === 'localhost'
-const proxy = isLocal ? '' : 'https://pre-onboarding-10th-6-week2.vercel.app'
+const proxy = isLocal ? '' : 'https://api.clinicaltrialskorea.com'
 
 export const getDiseases = (name: string): Promise<Disease[]> =>
   instance.get(`${proxy}/api/v1/search-conditions/`, {
